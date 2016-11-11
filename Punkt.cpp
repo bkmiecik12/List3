e66af3e59dfe796168f0367d4ca0fcdad1994bec
bkmiecik12 <bkmiecik12@gmail.com>
2016-11-11 13:20:03 +0100

    Initial commit

---
+++ Punkt.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+Punkt::Punkt(int a, int b)
+{
+	x=a;
+	y=b;
+}
+
+Punkt::~Punkt(){}
---
+++ Punkt.h
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+class Punkt
+{
+	private:
+	int x;
+	int y;
+	public:
+	Punkt(int,int);
+	~Punkt();
+}
